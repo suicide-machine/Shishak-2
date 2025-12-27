816fc9dec6695decf9ee9dd061c2413cd98a9d91
Cenote <himangshukalita131@gmail.com>
2025-12-27 12:51:34 +0530

    Zustand setup with auth api part-02

--- frontend/store/authStore.ts
+++ frontend/store/authStore.ts
@@ -1,5 +1,6 @@
 import { User } from "@/lib/types"
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
 interface AuthState {
   user: User | null
@@ -7,4 +8,49 @@ interface AuthState {
   loading: boolean
   error: string | null
   isAuthenticated: boolean
+
+  //Actions
+  setUser: (user: User, token: string) => void
+  clearError: () => void
+  logout: () => void
+
+  //Api Actions
+  loginTeacher: (email: string, password: string) => Promise<void>
+  loginStudent: (email: string, password: string) => Promise<void>
+  registerTeacher: (data: any) => Promise<void>
+  registerStudent: (data: any) => Promise<void>
+  fetchProfile: () => Promise<User | null>
+  updateProfile: (data: any) => Promise<void>
 }
+
+export const userAuthStore = create<AuthState>()(
+  persist((set, get) => ({
+    user: null,
+    token: null,
+    loading: false,
+    error: null,
+    isAuthenticated: false,
+
+    setUser: (user, token) => {
+      set({
+        user,
+        token,
+        isAuthenticated: true,
+        error: null,
+      })
+      localStorage.setItem("token", token)
+    },
+
+    clearError: () => set({ error: null }),
+
+    logout: () => {
+      localStorage.removeItem("token")
+      set({
+        user: null,
+        token: null,
+        isAuthenticated: false,
+        error: null,
+      })
+    },
+  }))
+)
