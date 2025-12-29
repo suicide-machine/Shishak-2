96cfb80ddc242a2781948b4b2f90c9009ee0dd19
Cenote <himangshukalita131@gmail.com>
2025-12-29 13:40:03 +0530

    Designing Auth Pages (Login/Signup) part-01

--- frontend/components/auth/AuthForm.tsx
+++ frontend/components/auth/AuthForm.tsx
@@ -1,4 +1,6 @@
-import React from "react"
+import { userAuthStore } from "@/store/authStore"
+import { useRouter } from "next/navigation"
+import React, { useState } from "react"
 
 interface AuthFormProps {
   type: "login" | "signup"
@@ -6,6 +8,62 @@ interface AuthFormProps {
 }
 
 const AuthForm = ({ type, userRole }: AuthFormProps) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [agreeToTerms, setAgreeToTerms] = useState(false)
+
+  const {
+    registerStudent,
+    registerTeacher,
+    loginStudent,
+    loginTeacher,
+    loading,
+    error,
+  } = userAuthStore()
+
+  const router = useRouter()
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if (type === "signup" && !agreeToTerms) return
+
+    try {
+      if (type === "signup") {
+        if (userRole === "teacher") {
+          await registerTeacher({
+            name: formData.name,
+            email: formData.email,
+            password: formData.password,
+          })
+        } else {
+          await registerStudent({
+            name: formData.name,
+            email: formData.email,
+            password: formData.password,
+          })
+        }
+
+        router.push(`/onboarding/${userRole}`)
+      } else {
+        if (userRole === "teacher") {
+          await loginTeacher(formData.email, formData.password)
+          router.push("/teacher/dashboard")
+        } else {
+          await loginStudent(formData.email, formData.password)
+          router.push("/student/dashboard")
+        }
+      }
+    } catch (error) {
+      console.log(error)
+      console.error(`${type} failed:`, error)
+    }
+  }
+
   return <div>AuthForm</div>
 }
 
