c3e64528e74be2c1502b03d7ae201ec0d5a0e4d1
Cenote <himangshukalita131@gmail.com>
2026-01-21 13:44:45 +0530

    Booking Page UI part-03

--- frontend/app/student/booking/[teacherId]/page.tsx
+++ frontend/app/student/booking/[teacherId]/page.tsx
@@ -43,6 +43,39 @@ const Page = () => {
     }
   }, [selectedDate, teacherId, fetchBookedSlots])
 
+  //Generate avaiable dates
+  useEffect(() => {
+    if (currentTeacher?.availabilityRange) {
+      const startDate = new Date(currentTeacher?.availabilityRange.startDate)
+      //Convert doctor's start date string into a Date Object
+
+      const endDate = new Date(currentTeacher?.availabilityRange.endDate)
+      //Convert doctor's end date string into a Date Object
+
+      const today = new Date()
+      today.setHours(0, 0, 0, 0)
+      //get today's date and reset time to midnight
+
+      const dates: string[] = []
+      //Empty list to hold avaiable dates
+
+      const iterationStart = new Date(
+        Math.max(today.getTime(), startDate.getTime()),
+      )
+
+      for (
+        let d = new Date(iterationStart);
+        d <= endDate && dates.length < 90;
+        d.setDate(d.getDate() + 1)
+      ) {
+        dates.push(toLocalYMD(d))
+        //Convert date into YYYY-MM-DD format and add to list
+      }
+
+      setAvailableDates(dates)
+    }
+  }, [currentTeacher])
+
   return <div>Page</div>
 }
 
