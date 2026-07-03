3fc5c815320f0cb7974b0078472808cae1c19e17
Cenote <himangshukalita131@gmail.com>
2026-07-04 01:41:22 +0530

    Zegocloud sdk integration part-02

--- frontend/components/call/AppointmentCall.tsx
+++ frontend/components/call/AppointmentCall.tsx
@@ -1,5 +1,6 @@
 import { Appointment } from "@/store/appointmentStore"
-import React, { useRef } from "react"
+import React, { useCallback, useRef } from "react"
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt"
 
 interface AppointmentCallInterface {
   appointment: Appointment
@@ -22,6 +23,148 @@ const AppointmentCall = ({
   const containerRef = useRef<HTMLDivElement>(null)
   const initializationRef = useRef(false)
   const isComponentMountedRef = useRef(true)
+
+  const memoizedJoinConsultation = useCallback(
+    async (appointmentId: string) => {
+      await joinAppointment(appointmentId)
+    },
+    [joinAppointment],
+  )
+
+  const intializeCall = useCallback(
+    async (container: HTMLDivElement) => {
+      if (
+        initializationRef.current ||
+        zpRef.current ||
+        !isComponentMountedRef.current
+      ) {
+        return
+      }
+
+      if (!container || !container.isConnected) {
+        return
+      }
+
+      try {
+        initializationRef.current = true
+        const appId = process.env.NEXT_PUBLIC_ZEGOCLOUD_APP_ID
+        const serverSecret = process.env.NEXT_PUBLIC_ZEGOCLOUD_SERVER_SECRET
+
+        if (!appId || !serverSecret) {
+          throw new Error("Zegocloud credentials not configured")
+        }
+
+        const numericAppId = Number.parseInt(appId)
+
+        if (isNaN(numericAppId)) {
+          throw new Error("Invalid Zegocloud App Id")
+        }
+
+        try {
+          await memoizedJoinConsultation(appointment?._id)
+        } catch (error) {
+          console.warn("failed to update appointment", error)
+        }
+
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+          numericAppId,
+          serverSecret,
+          appointment.zegoRoomId,
+          currentUser.id,
+          currentUser.name,
+        )
+
+        const zp = ZegoUIKitPrebuilt.create(kitToken)
+        zpRef.current = zp
+
+        const isVideoCall = appointment.appointmentType === "Video Appointment"
+
+        zp.joinRoom({
+          container,
+          scenario: {
+            mode: ZegoUIKitPrebuilt.OneONoneCall,
+          },
+          turnOnMicrophoneWhenJoining: true,
+          showMyMicrophoneToggleButton: true,
+          turnOnCameraWhenJoining: isVideoCall,
+          showMyCameraToggleButton: isVideoCall,
+          showScreenSharingButton: true,
+          showTextChat: true,
+          showUserList: true,
+          showRemoveUserButton: true,
+          showPinButton: false,
+          showAudioVideoSettingsButton: true,
+          showTurnOffRemoteCameraButton: true,
+          showTurnOffRemoteMicrophoneButton: true,
+          maxUsers: 2,
+          layout: "Auto",
+          showLayoutButton: false,
+          onJoinRoom: () => {
+            if (isComponentMountedRef.current) {
+              console.log(
+                `Joined ${appointment.appointmentType} : ${appointment.zegoRoomId}`,
+              )
+            }
+          },
+
+          onLeaveRoom: () => {
+            if (isComponentMountedRef.current) {
+              if (zpRef.current) {
+                try {
+                  zpRef.current.mutePublishStreamAudio(true)
+                  zpRef.current.mutePublishStreamVideo(true)
+                } catch (error) {
+                  console.warn("Error turning off camera/mircophone")
+                }
+              }
+            }
+          },
+
+          onUserJoin: (users: any[]) => {
+            if (isComponentMountedRef.current) {
+              console.log("Users Joined", users)
+            }
+          },
+
+          onUserLeave: (users: any[]) => {
+            if (isComponentMountedRef.current) {
+              console.log("Users left", users)
+            }
+          },
+
+          showLeavingView: true,
+
+          onReturnToHomeScreenClicked: () => {
+            if (zpRef.current) {
+              try {
+                zpRef.current.mutePublishStreamAudio(true)
+                zpRef.current.mutePublishStreamVideo(true)
+              } catch (error) {
+                console.warn("Error turning off camera/mircophone")
+              }
+            }
+            onCallEnd()
+          },
+        })
+      } catch (error) {
+        console.error("Call Initilization failed", error)
+        initializationRef.current = false
+        if (isComponentMountedRef.current) {
+          zpRef.current = null
+          onCallEnd()
+        }
+      }
+    },
+    [
+      appointment?._id,
+      appointment.zegoRoomId,
+      appointment.appointmentType,
+      currentUser.id,
+      currentUser.name,
+      memoizedJoinConsultation,
+      onCallEnd,
+    ],
+  )
   return <div>AppointmentCall</div>
 }
 
